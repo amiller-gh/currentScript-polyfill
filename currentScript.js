713564790783b0c3324e1da2ed6147f9d4eb3bc9
Adam Miller <adam@collegia.org>
2015-12-11 11:21:36 -0800

    Update currentScript.js

--- currentScript.js
+++ currentScript.js
@@ -6,7 +6,7 @@
   var currentScript = "currentScript",
       scripts = document.getElementsByTagName('script'); // Live NodeList collection
 
-  // If browser needs currentScript polyfill
+  // If browser needs currentScript polyfill, add get currentScript() to the document object
   if (!(currentScript in document)) {
     Object.defineProperty(document, currentScript, {
       get: function(){
@@ -17,11 +17,11 @@
         catch (err) {
 
           // Find the second match for the "at" string to get file src url from stack.
-          // Specifically works the format of stack traces in IE.
+          // Specifically works with the format of stack traces in IE.
           currentScript = / *at [^\(]\((.*):.+:\).+/ig;
           err = currentScript.exec(err.stack || '') && currentScript.exec(err.stack)[1];
 
-          // For all scripts, if src matches or if ready state is interactive, return the script tag
+          // For all scripts on the page, if src matches or if ready state is interactive, return the script tag
           for(i in scripts){
             if(scripts[i].src == err || scripts[i].readyState == "interactive"){
               err = scripts[i];
